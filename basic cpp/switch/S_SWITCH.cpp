692d05de124c6b4b5e915362a879962855c3b387
Shreya Christiana <5hre9a@gmail.com>
2021-05-13 23:43:15 +0530

    C++

---
+++ basic cpp/switch/S_SWITCH.cpp
@@ -0,0 +1,38 @@
+// switch
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		
+	int n;
+	cin>>n;
+	
+	switch(n)
+	{
+		case 10 : cout<<"value is 10";
+		break;
+		
+		case 15 : cout<< "value is 15";
+		break;
+		
+		default : cout<< "not found";
+	}
+	
+}
+	return 0;
+}
+
+
+
+//3
+//10
+//value is 10
+//16
+//not found
+//15
+//value is 15
