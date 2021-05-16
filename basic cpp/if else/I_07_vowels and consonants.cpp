12a0d609e95b38d14e8985862f7f3d1b12f45898
Shreya Christiana <5hre9a@gmail.com>
2021-05-17 00:28:40 +0530

    C++

---
+++ basic cpp/if else/I_07_vowels and consonants.cpp
@@ -0,0 +1,39 @@
+//author :shreyamalogi
+
+/*
+
+
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    int isLowercaseVowel, isUppercaseVowel;
+
+    cout << "Enter an alphabet: ";
+    cin >> c;
+
+    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    if (isLowercaseVowel || isUppercaseVowel)
+        cout << c << " is a vowel.";
+    else
+        cout << c << " is a consonant.";
+
+    return 0;
+}
+
+
+//Enter an alphabet: a
+//a is a vowel.
+
+//Enter an alphabet: k
+//k is a consonant.
+
