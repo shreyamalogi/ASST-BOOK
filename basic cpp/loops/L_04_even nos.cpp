aca15df94c0427dbab547de4c423f1e710948002
Shreya Christiana Malogi <5hre9a@gmail.com>
2022-10-10 04:06:26 +0530

    Update L_04_even nos.cpp

--- basic cpp/loops/L_04_even nos.cpp
+++ basic cpp/loops/L_04_even nos.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	for(int i=1; i<=10; i++) 		//equal to symbol is compulsory without which we get onnly 9 elements
 	{
-		cout<<i*2<<" ";  			//variable should be multiplied by 2 in order to get even
+		cout<<2*i<<" ";  		//formula: even = 2n, odd = 2n+1
 	}
 	return 0;
 }
