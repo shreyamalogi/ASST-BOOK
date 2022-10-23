11f8be9c07a21b714f2770a03bd1b3b7062a6a35
Shreya Christiana Malogi <5hre9a@gmail.com>
2022-10-23 23:09:14 +0530

    star patterns

---
+++ patterns/1_star patterns/03_right sided triangle.cpp
@@ -0,0 +1,38 @@
+//author @shreyamalogi
+//right sided triangle
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+
+int n;
+cin>>n;
+
+for(int i=1; i<=n; i++){
+	
+	for(int j=i; j<=n; j++){
+		cout<<"  ";
+	}
+	
+	for(int j=1; j<=i; j++){
+		cout<<"* ";
+	}
+	cout<<endl;
+}
+
+return 0;
+}
+
+
+//output
+
+//5
+//          *
+//        * *
+//      * * *
+//    * * * *
+//  * * * * *
