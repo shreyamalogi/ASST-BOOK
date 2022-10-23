4f69ec0d1710cac330b5f5b398fb98c531a5c58c
Shreya Christiana Malogi <5hre9a@gmail.com>
2022-10-23 23:26:26 +0530

    star patterns

---
+++ patterns/1_star patterns/07_diamond.cpp
@@ -0,0 +1,62 @@
+//author @shreyamalogi
+//diamond pattern (hill+reverse hill)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+
+int n;
+cin>>n;
+
+for(int i=1; i<=n;i++){
+	for(int j=i; j<=n ; j++){
+		cout<<"  ";
+	}
+	for(int j=1; j<i; j++){
+		cout<<"* ";
+	}
+	for(int j=1; j<=i; j++){
+		cout<<"* ";
+	}
+	
+	
+	cout<<endl;
+}
+
+
+for(int i=1; i<=n; i++){
+	for(int j=1 ; j<=i; j++){
+		cout<<"  ";
+	}
+	for(int j=i ; j<n; j++){
+		cout<<"* ";
+	}
+	for(int j=i ; j<=n; j++){
+		cout<<"* ";
+	}
+	cout<<endl;
+}
+
+
+return 0;
+}
+
+
+
+
+//5
+//          *
+//        * * *
+//      * * * * *
+//    * * * * * * *
+//  * * * * * * * * *
+//  * * * * * * * * *
+//    * * * * * * *
+//      * * * * *
+//        * * *
+//          *
+
