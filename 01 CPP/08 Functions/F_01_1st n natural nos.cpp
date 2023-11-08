a261a56308c7bd0ff5292f49bf63938c213c32e8
Shreya Malogi <5hre9a@gmail.com>
2023-11-09 00:12:26 +0530

    Update F_01_1st n natural nos.cpp

--- 01 CPP/08 Functions/F_01_1st n natural nos.cpp
+++ 01 CPP/08 Functions/F_01_1st n natural nos.cpp
@@ -6,12 +6,26 @@
 
 using namespace std;
 
+//eg: ans += i; is like your friend keeping track of the total number of candies. 
+//The += means "add and update." So, every time your friend sees a new candy (represented by i), 
+//he adds it to his total.
+
+
+// int sum(int n) {
+//     // The formula for the sum of the first n natural numbers is n * (n + 1) / 2
+//     return n * (n + 1) / 2;
+// }
+
+
+// ans = ans + i
+
 int sum(int n){
 	int ans =0;
 	for(int i=1;i<=n;i++)
 		ans += i;
 	return ans;
 }
+
 
 int main()
 {
